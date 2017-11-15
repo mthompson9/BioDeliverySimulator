4e14278fc425b809d86e66f4b26d08b21a5eb584
cconnolly9 <32094732+cconnolly9@users.noreply.github.com>
2017-11-15 16:20:45 +0000

    Auto populate from db all values except nozzle drop down

--- app.js
+++ app.js
@@ -9,21 +9,25 @@ var rainfall;
 var relativeHumidity;
 var windSpeed;
 
-function loadDefaults(){
-
-    
-
+function loadSprayDefaults(){
 
     var db = firebase.database();
     var ref = db.ref('/Defaults/Spray');
     ref.once('value', function(snapshot) {
 
-
-            applicationRate = snapshot.val().appRate
+        windSpeed = snapshot.val().windspeed.low
+        rainfall = snapshot.val().rainfall.weak
+        dilutionRate = snapshot.val().dilutionRate
+        relativeHumidity = snapshot.val().relativeHumidity.humid
+            applicationRate = snapshot.val().applicationRate
             applicationHeight = snapshot.val().applicationHeight.boonspray
             window.alert(applicationRate)
             window.alert(applicationHeight)
 
+            document.getElementById("low").checked = windSpeed;
+            document.getElementById("weak").checked = rainfall;
+            document.getElementById("dilutionrate").checked = dilutionRate;
+            document.getElementById("humid").checked = relativeHumidity;
             document.getElementById("rate").value = applicationRate;
             document.getElementById("boonSpray").checked = applicationHeight;
             
