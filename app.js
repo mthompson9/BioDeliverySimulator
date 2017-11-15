a44eb71de16b4217c4f901b44587b13f2612681e
cconnolly9 <32094732+cconnolly9@users.noreply.github.com>
2017-11-15 16:49:25 +0000

    Spray completely auto populated, leaf props test

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var dropletDiameter;
 var rainfall;
 var relativeHumidity;
 var windSpeed;
+var nozzleSize;
+var nozzleLength;
 
 function loadSprayDefaults(){
 
@@ -17,29 +19,39 @@ function loadSprayDefaults(){
 
         windSpeed = snapshot.val().windspeed.low
         rainfall = snapshot.val().rainfall.weak
+        nozzleSize = snapshot.val().nozzle.small
+        nozzleLength = snapshot.val().nozzle.long
         dilutionRate = snapshot.val().dilutionRate
         relativeHumidity = snapshot.val().relativeHumidity.humid
             applicationRate = snapshot.val().applicationRate
             applicationHeight = snapshot.val().applicationHeight.boonspray
-            window.alert(applicationRate)
-            window.alert(applicationHeight)
 
             document.getElementById("low").checked = windSpeed;
             document.getElementById("weak").checked = rainfall;
-            document.getElementById("dilutionrate").checked = dilutionRate;
+            document.getElementById("dilutionrate").value = dilutionRate;
+            document.getElementById('nozzles').getElementsByTagName('option')[1].selected = 'selected'
+            document.getElementById("size").value = nozzleSize;
+            document.getElementById("length").value = nozzleLength;
             document.getElementById("humid").checked = relativeHumidity;
             document.getElementById("rate").value = applicationRate;
             document.getElementById("boonSpray").checked = applicationHeight;
             
-            
+    })
+
+}
+
+function loadLeafPropDefaults(){
+
+    var db = firebase.database();
+    var ref = db.ref('/Defaults/Leaf Properties');
+    ref.once('value', function(snapshot) {
+
+        var test = snapshot.val().metabolicRate;
+
+        window.alert(test)
+
 
 
     })
-
-
-
-    
-
-
 
 }
